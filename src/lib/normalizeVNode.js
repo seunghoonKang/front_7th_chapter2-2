1a9dd96ed8e7f785db78cca6ef66eac6d3bb4734
seunghoonKang <tmdgns4321@gmail.com>
2025-11-21 02:16:25 +0900

    feat - normalizeVNode

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,4 +1,5 @@
 export function normalizeVNode(vNode) {
+  console.log(vNode);
   // null, undefined, boolean 값은 빈 문자열로 변환
   if (
     vNode === null ||
@@ -6,31 +7,31 @@ export function normalizeVNode(vNode) {
     vNode === true ||
     vNode === false
   ) {
-    return "";
+    return '';
   }
 
   // 문자열과 숫자는 문자열로 변환
-  if (typeof vNode === "string" || typeof vNode === "number") {
+  if (typeof vNode === 'string' || typeof vNode === 'number') {
     return String(vNode);
   }
 
   // 컴포넌트 정규화
   // 받은 값이 컴포넌트면, 실행해서 재귀해서 정규화한 값을 반환한다.
   // { type: [Function: TestComponent], props: null, children: [] }
-  if (typeof vNode.type === "function") {
+  if (typeof vNode.type === 'function') {
     return normalizeVNode(
-      vNode.type({ ...vNode.props, children: vNode.children }),
+      vNode.type({ ...vNode.props, children: vNode.children })
     );
   }
-  if (typeof vNode.type === "string") {
+  if (typeof vNode.type === 'string') {
     // children 배열을 정규화하고 빈 문자열 제거
     const normalizedChildren = vNode.children
       .map((child) => normalizeVNode(child))
-      .filter((child) => child !== "");
+      .filter((child) => child !== '');
 
     return {
       ...vNode,
-      children: normalizedChildren,
+      children: normalizedChildren
     };
   }
 
@@ -42,7 +43,7 @@ export function normalizeVNode(vNode) {
         child !== null &&
         child !== undefined &&
         child !== false &&
-        child !== true,
+        child !== true
     );
   return { ...vNode, children: flatFilterChildren };
 }
