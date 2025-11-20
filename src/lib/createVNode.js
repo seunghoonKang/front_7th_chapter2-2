12d11398f0d651b6e2bd973ab275144aed40ddda
seunghoonKang <tmdgns4321@gmail.com>
2025-11-21 02:16:08 +0900

    feat - createVNode

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -2,7 +2,7 @@ export function createVNode(type, props, ...children) {
   const flatFilterChildren = children
     .flat(Infinity)
     .filter(
-      (child) => child !== null && child !== undefined && child !== false,
+      (child) => child !== null && child !== undefined && child !== false
     );
   return { type, props, children: flatFilterChildren };
 }
