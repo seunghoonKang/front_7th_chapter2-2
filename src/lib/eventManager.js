290cb4bd964432787493df01f156ca9f47066773
seunghoonKang <tmdgns4321@gmail.com>
2025-11-21 02:17:03 +0900

    feat- eventManager

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,51 @@
-export function setupEventListeners(root) {}
+export function addEvent(element, eventType, handler) {
+  // element에 __handlers 객체 없으면 생성
+  if (!element.__handlers) {
+    element.__handlers = {};
+  }
 
-export function addEvent(element, eventType, handler) {}
+  // 해당 이벤트 타입 배열 없으면 생성
+  if (!element.__handlers[eventType]) {
+    element.__handlers[eventType] = [];
+  }
 
-export function removeEvent(element, eventType, handler) {}
+  // 핸들러 등록
+  element.__handlers[eventType].push(handler);
+}
+
+export function removeEvent(element, eventType, handler) {
+  if (!element.__handlers || !element.__handlers[eventType]) {
+    return;
+  }
+
+  // 해당 핸들러 제거
+  element.__handlers[eventType] = element.__handlers[eventType].filter(
+    (h) => h !== handler
+  );
+}
+
+export function setupEventListeners(root) {
+  [
+    'click',
+    'mouseover',
+    'focus',
+    'keydown',
+    'input',
+    'change',
+    'submit',
+    'blur'
+  ].forEach((eventType) => {
+    root.addEventListener(eventType, (e) => {
+      let target = e.target;
+
+      while (target && target !== root) {
+        if (target.__handlers && target.__handlers[eventType]) {
+          target.__handlers[eventType].forEach((handler) => {
+            handler(e);
+          });
+        }
+        target = target.parentNode;
+      }
+    });
+  });
+}
